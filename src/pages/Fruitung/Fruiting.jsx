d59b0554a8420fe1f91f6d53f3fdaf0fb597fa38
yunssup <btob031541@gmail.com>
2024-02-22 19:53:55 +0900

    fix : 프루잉 파트 전체 수정 완료

--- src/pages/Fruitung/Fruiting.jsx
+++ src/pages/Fruitung/Fruiting.jsx
@@ -1,167 +1,93 @@
-import React from "react";
-
-import {
-  BackGround,
-  Title,
-  Box,
-  ContactBoxBut,
-  Story,
-  Hi,
-  Image,
-  Logo,
-  TimeBox,
-  MonthBox,
-  InfoBoxBottom1,
-  InfoBoxTop1,
-  InfoBoxBottom2,
-  InfoBoxTop2,
-  InfoBoxContainer,
-  ImageWe,
-  BoxWe,
-  Image2,
-  Image1,
-  Run,
-} from "./Style";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { useState } from "react";
-
-import {
-  faPen,
-  faEnvelope,
-  faComment,
-} from "@fortawesome/free-solid-svg-icons";
-import { faInstagram } from "@fortawesome/free-brands-svg-icons";
-import Navbar from "../../components/Navbar/Navbar";
-import { Link } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import * as S from "./Style";
+import "aos/dist/aos.css";
+import AOS from "aos";
 
 function Fruiting() {
-  const [isHoveredTop1, setIsHoveredTop1] = useState(false);
-  const [isHoveredTop2, setIsHoveredTop2] = useState(false);
-  const [isHoveredBottom1, setIsHoveredBottom1] = useState(false);
-  const [isHoveredBottom2, setIsHoveredBottom2] = useState(false);
+  useEffect(() => {
+    AOS.init({
+      duration: 1000,
+    });
+  }, []);
 
   return (
     <>
-      <BackGround>
+      <S.BackGround>
         {/* 1페이지 */}
-        <Hi>안녕하세요, 프루잉입니다!</Hi>
-        <Logo>
-          <Image src="Image/동국대로고.png" alt="수정중.." />
-          <Image1 src="Image/프루잉로고.png" alt="프루잉 이미지" />
-          <Image2 src="Image/인액로고.png" alt="프루잉 이미지" />
-        </Logo>
-        <Story>
-          <p>
-            프루잉은 동국대학교 소셜벤처 창업동아리 인액터스 소속으로,
-            비즈니스를 통해 사회 문제를 해결하기 위해 모인 대학생 창업팀입니다.{" "}
-          </p>
-          <p>
-            프루잉은 폐지 수거 어르신의 경제적 문제에 주목하여 그분들이 겪고
-            계신 어려움을 해결하고자 합니다.
-          </p>
-          <p>
-            폐지 수거 일과 유사한 특성을 띄지만 안전하고 정당한 임금을 보장할 수
-            있는 일자리를 창출하기 위해 노력하였고,{" "}
-          </p>
-          <p>
-            그 결과 마을의 폐지를 수거하는 일 대신 마을 주민에게 과일컵을
-            배달하는 시니어 배송원 ‘프루잉’이 탄생하였습니다.
-          </p>
-        </Story>
+        <S.Hi data-aos="fade-up">안녕하세요, 프루잉입니다!</S.Hi>
+        <S.Logo>
+          <S.Image
+            data-aos="fade-up"
+            src="Image/동국대로고.png"
+            alt="수정중.."
+          />
+          <S.Image1
+            data-aos="fade-up"
+            src="Image/프루잉로고.png"
+            alt="프루잉 이미지"
+          />
+          <S.Image2
+            data-aos="fade-up"
+            src="Image/인액로고.png"
+            alt="프루잉 이미지"
+          />
+        </S.Logo>
+        <S.Story data-aos="fade-up">
+          프루잉은 동국대학교 소셜벤처 창업동아리 인액터스 소속으로, 비즈니스를
+          통해 사회 문제를 해결하기 위해 모인 대학생 창업팀입니다. <br />
+          프루잉은 폐지 수거 어르신의 경제적 문제에 주목하여 그분들이 겪고 계신
+          어려움을 해결하고자 합니다.
+          <br />
+          폐지 수거 일과 유사한 특성을 띄지만 안전하고 정당한 임금을 보장할 수
+          있는 일자리를 창출하기 위해 노력하였고, <br />그 결과, 매일 동네를
+          돌아다니며 폐지를 수집하는 대신 과일컵을 배달하는 새로운 일자리
+          창출하였습니다.
+        </S.Story>
         {/* 2페이지 */}
-        <Run>지금까지 프루잉은 이렇게 달려왔어요!</Run>
-        <InfoBoxContainer>
-          <InfoBoxTop1 className={isHoveredTop1 ? "animated" : ""}>
-            팀 빌딩
-            <br />
-            인액터스 프로젝트 아이디어 대회 동상 <br />
-            인액터스 솔루션 코칭 부트캠프 합격
-          </InfoBoxTop1>
-          <InfoBoxTop2 className={isHoveredTop2 ? "animated" : ""}>
-            과일컵 정기 배송 1차 체험단 진행
-            <br />
-            2023 동국 창업 위크 참여 <br />
-          </InfoBoxTop2>
-        </InfoBoxContainer>
-        <TimeBox>
-          <MonthBox
-            onMouseEnter={() => setIsHoveredTop1(true)}
-            onMouseLeave={() => setIsHoveredTop1(false)}
-            className={isHoveredTop1 ? "animated" : ""}
-          >
-            4~6월
-          </MonthBox>
-          <MonthBox
-            onMouseEnter={() => setIsHoveredBottom1(true)}
-            onMouseLeave={() => setIsHoveredBottom1(false)}
-            className={isHoveredBottom1 ? "animated" : ""}
-          >
-            7~8월
-          </MonthBox>
-          <MonthBox
-            onMouseEnter={() => setIsHoveredTop2(true)}
-            onMouseLeave={() => setIsHoveredTop2(false)}
-            className={isHoveredTop2 ? "animated" : ""}
-          >
-            9~10월
-          </MonthBox>
-          <MonthBox
-            onMouseEnter={() => setIsHoveredBottom2(true)}
-            onMouseLeave={() => setIsHoveredBottom2(false)}
-            className={isHoveredBottom2 ? "animated" : ""}
-          >
-            11~12월
-          </MonthBox>
-        </TimeBox>
-        <InfoBoxContainer>
-          <InfoBoxBottom1 className={isHoveredBottom1 ? "animated" : ""}>
-            푸릇해 사당역점 협업 성사
-            <br />
-            (사)함께 만드는 세상 "자주모여" 지원 대상 선정
-          </InfoBoxBottom1>{" "}
-          <InfoBoxBottom2 className={isHoveredBottom2 ? "animated" : ""}>
-            대학생 esg 청년 창업 아이디어 공모전 우수상
-            <br />
-            과일컵 정기 배송 2차 체험단 진행 <br />
-          </InfoBoxBottom2>
-        </InfoBoxContainer>
+        <S.Hi data-aos="fade-up">지금까지 프루잉은 이렇게 달려왔어요!</S.Hi>
+        <S.ImagePlan
+          data-aos="fade-right"
+          src="Image/라인수정.png"
+          alt="수정중.."
+        />
+
         {/* 3페이지 */}
-        <Hi>프루잉의 팀원들을 소개할게요</Hi>
-        <Box>
-          <ImageWe src="Image\유빈.png" alt="유빈 사진 수정..." />
-          <BoxWe>
+        <S.Hi data-aos="fade-up">프루잉의 팀원들을 소개할게요</S.Hi>
+        <S.Box data-aos="fade-right">
+          <S.ImageWe src="Image\유빈.png" alt="유빈 사진 수정..." />
+          <S.BoxWe>
             PM: 진유빈 (교육학/경영학)
             <br /> 프루잉의 PM으로서 프로젝트 전반을 관리하며 팀을 이끌어나가요!
-          </BoxWe>
-        </Box>
-        <Box>
-          <BoxWe>
+          </S.BoxWe>
+        </S.Box>
+        <S.Box data-aos="fade-left">
+          <S.BoxWe>
             상품 기획 : 권두윤 (식품생명공학) <br /> 더 나은 일자리를 위해
             고객과 프루잉을 연결할 수 있는 상품을 기획해요!
-          </BoxWe>
-          <ImageWe src="Image/두윤.png" alt="프루잉 이미지" />
-        </Box>
-        <Box>
-          <ImageWe src="Image/유정.png" alt="프루잉 이미지" />
-          <BoxWe>
+          </S.BoxWe>
+          <S.ImageWe src="Image/두윤.png" alt="프루잉 이미지" />
+        </S.Box>
+        <S.Box data-aos="fade-right">
+          <S.ImageWe src="Image/유정.png" alt="프루잉 이미지" />
+          <S.BoxWe>
             회계/재무 : 윤유정 (회계학) <br /> 지속 가능한 비즈니스를 위한 재무
             및 원가회계 업무를 담당하고 있어요!
-          </BoxWe>
-        </Box>
-        <Box>
-          <BoxWe>
+          </S.BoxWe>
+        </S.Box>
+        <S.Box data-aos="fade-left">
+          <S.BoxWe>
             서비스 개발 : 김윤서 (경영학/ 컴퓨터공학) <br /> 많은 분들이
             프루잉에 대해 알 수 있도록 프루잉의 웹서비스를 개발해요!
-          </BoxWe>
-          <ImageWe src="Image/윤서.png" alt="프루잉 이미지" />
-        </Box>
-        <Box>
-          <ImageWe src="Image/준희.png" alt="프루잉 이미지" />
-          <BoxWe>
-            경영/기획 : 나준희 (국제통상학) <br /> 지속 가능한 비즈니스를 위한
-            재무 및 원가회계 업무를 담당하고 있어요!
-          </BoxWe>
-        </Box>
+          </S.BoxWe>
+          <S.ImageWe src="Image/윤서.png" alt="프루잉 이미지" />
+        </S.Box>
+        <S.Box data-aos="fade-right">
+          <S.ImageWe src="Image/준희.png" alt="프루잉 이미지" />
+          <S.BoxWe>
+            경영/기획 : 나준희 (국제통상학) <br /> 안정적인 일자리 창출과 더
+            나은 프루잉을 만들기 위해 경영과 기획을 담당하고 있어요!
+          </S.BoxWe>
+        </S.Box>
         {/* <Box>
         <BoxWe>
           디자인 / 마케팅 : 김진영 (경영학) <br /> 지속 가능한 비즈니스를 위한
@@ -210,7 +136,7 @@ function Fruiting() {
             </ContactBoxBut>
           </Link>
         </Box> */}
-      </BackGround>
+      </S.BackGround>
     </>
   );
 }
--- src/pages/Fruitung/Style.jsx
+++ src/pages/Fruitung/Style.jsx
@@ -2,40 +2,30 @@
 import styled from "styled-components";
 
 export const BackGround = styled.div`
-  /* height: 100vh; */
   width: 100vw;
   background: #fff;
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  overflow-x: hidden; /* 가로 스크롤 숨김 */
 `;
-export const Title = styled.div`
-  color: #000;
-  font-size: 40px;
-  font-style: normal;
-  font-weight: 600;
-  line-height: normal;
-  font-family: "Black Han Sans", sans-serif;
-  margin: 1% 0%;
-`;
+
 export const Hi = styled.div`
   color: #000;
-  font-family: "Black Han Sans", sans-serif;
-  font-size: 40px;
+  font-family: Pretendard-Bold;
+  font-size: 36px;
   font-style: normal;
   font-weight: 600;
   line-height: normal;
-  margin-top: 5%;
+  margin-top: 3.5%;
 `;
 export const Story = styled.div`
   color: #000;
   font-size: 21px;
   font-style: normal;
-  font-family: "Black Han Sans", sans-serif;
+  font-family: Pretendard-Medium;
   text-align: center;
-  font-weight: 400;
+  line-height: 2.5;
 `;
 export const Logo = styled.div`
   display: flex;
@@ -60,164 +50,10 @@ export const Image2 = styled.img`
   margin: 23px auto;
   margin-top: 65px;
 `;
-export const Run = styled.div`
-  color: #000;
-  font-family: "Black Han Sans", sans-serif;
-  font-size: 40px;
-  font-style: normal;
-  font-weight: 600;
-  line-height: normal;
-  margin: 1% 0%;
-`;
-export const TimeBox = styled.div`
-  width: 60%;
-  height: 60px;
-  background-color: #f5f5f5;
-  border-radius: 50px;
-  display: flex;
-  align-items: center;
-  justify-content: space-between; /* 요소 사이에 공간을 동일하게 배분합니다 */
-  /* padding: 0 20px; */
-  color: #000;
-  font-size: 20px;
-  font-weight: bold;
-  box-shadow: 0px 4px 4px #00000040;
-`;
 
-export const MonthBox = styled.div`
-  width: 18%;
-  height: 60px;
-  background-color: #ffa100;
-  border-radius: 50px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  color: #ffffff;
-  font-size: 20px;
-  font-weight: bold;
-  box-shadow: 0px 4px 4px #00000040;
-
-  &:first-child {
-    margin-left: 0; /* 첫 번째 MonthBox의 왼쪽 마진을 0으로 설정 */
-  }
-
-  &:last-child {
-    margin-right: 0; /* 마지막 MonthBox의 오른쪽 마진을 0으로 설정 */
-  }
-
-  cursor: pointer;
-  transition: transform 0.3s ease-in-out;
-  transform-origin: center;
-  transform: scale(1);
-
-  /* &.animated {
-    transform: scale(1.1);
-  } */
-`;
-
-export const InfoBoxTop1 = styled.div`
-  display: flex;
-  width: 22%;
-  height: 120px;
-  background-color: #ffffff;
-  border-radius: 40px;
-  align-items: center;
-  justify-content: center;
-  color: #000;
-  font-size: 15px;
-  font-weight: bold;
-  box-shadow: 0px 4px 4px #00000040;
-  line-height: 200%;
-  border: 2px solid #ffa100;
-  margin-left: 20%;
-
-  transition: transform 0.5s ease-in-out;
-  transform-origin: center;
-  transform: scale(1);
-
-  &.animated {
-    transform: scale(1.1);
-  }
-`;
-export const InfoBoxTop2 = styled.div`
-  display: flex;
-  width: 22%;
-  height: 120px;
-  background-color: #ffffff;
-  border-radius: 40px;
-  align-items: center;
-  justify-content: center;
-  color: #000;
-  font-size: 15px;
-  font-weight: bold;
-  box-shadow: 0px 4px 4px #00000040;
-  line-height: 200%;
-  border: 2px solid #ffa100;
-  margin-right: 25%;
-
-  transition: transform 0.5s ease-in-out;
-  transform-origin: center;
-  transform: scale(1);
-
-  &.animated {
-    transform: scale(1.1);
-  }
-`;
-export const InfoBoxBottom1 = styled.div`
-  display: flex;
-  width: 25%;
-  height: 120px;
-  background-color: #ffffff;
-  border-radius: 40px;
-  align-items: center;
-  justify-content: center;
-  color: #000;
-  font-size: 15px;
-  font-weight: bold;
-  box-shadow: 0px 4px 4px #00000040;
-  line-height: 200%;
-  border: 2px solid #ffa100;
-  margin-left: 37%;
-
-  transition: transform 0.5s ease-in-out;
-  transform-origin: center;
-  transform: scale(1);
-
-  &.animated {
-    transform: scale(1.1);
-  }
-`;
-export const InfoBoxBottom2 = styled.div`
-  display: flex;
-  width: 24%;
-  height: 120px;
-  background-color: #ffffff;
-  border-radius: 40px;
-  align-items: center;
-  justify-content: center;
-  color: #000;
-  font-size: 15px;
-  font-weight: bold;
-  box-shadow: 0px 4px 4px #00000040;
-  line-height: 200%;
-  border: 2px solid #ffa100;
-  margin-right: 6%;
-
-  transition: transform 0.5s ease-in-out;
-  transform-origin: center;
-  transform: scale(1);
-
-  &.animated {
-    transform: scale(1.1);
-  }
-`;
-export const InfoBoxContainer = styled.div`
-  width: 100%;
-  height: 120px;
-  display: flex;
-  justify-content: center;
-  margin: 2% 0%;
-  justify-content: space-between;
+export const ImagePlan = styled.img`
+  width: 70%;
+  margin: 2%;
 `;
 
 export const Box = styled.div`
@@ -257,7 +93,7 @@ export const ContactBoxBut = styled.button`
   box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
   border: 2px solid #ffa100;
   color: #000;
-  font-family: Inter;
+  font-family: Pretendard-Medium;
   font-size: 20px;
   font-style: normal;
   font-weight: 550;
